3323e6581f2a3a6198c91d3dc1723cfd05dd9386
Nguyen Van Phat (VN.OP-ENG) <VanPhat.Nguyen01@ttigroup.com.vn>
2023-08-02 11:36:12 +0700

    finally tiktok

--- main.js
+++ main.js
@@ -192,12 +192,21 @@ Object.assign(swiperEl3, {
       spaceBetween: 20,
     },
     768: {
-      slidesPerView: 2,
+      slidesPerView: 1,
       spaceBetween: 40,
     },
     1024: {
-      slidesPerView: 3,
+      slidesPerView: 1,
       spaceBetween: 50,
+    },
+    
+    1320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1730: {
+      slidesPerView: 3,
+      spaceBetween: 20,
     },
   },
 });
